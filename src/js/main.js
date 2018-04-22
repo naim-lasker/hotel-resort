206e00d33c7b74a88d4de061dbc0d6d617102b51
Naim Lasker <naimlasker@live.com>
2018-04-23 02:47:47 +0600

    Homepage done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 "use strict";
 
 import '../sass/style.scss';
+
+AOS.init();
+
+// $(window).on('scroll', function(event) {
+//     var scrollValue = $(window).scrollTop();
+//     if (scrollValue == settings.scrollTopPx || scrollValue > 70) {
+//          $('.navbar').addClass('navbar');
+//     } 
+// });
