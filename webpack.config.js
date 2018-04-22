d0fe62f8519b411b85455b5e651ebfc4ce0fdaa7
Naim Lasker <naimlasker@live.com>
2018-04-22 12:25:30 +0600

    node packages

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: path.resolve(__dirname, 'src/js', 'main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'bundle.css'
+        })
+    ]
+}
